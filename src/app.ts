4000d762c97c1e4ec8ebcb2b3259a992163ff940
Kaique Demetrio <160547555+kaiquedm12@users.noreply.github.com>
2025-06-14 13:20:14 -0300

    Mesclando repositórios

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import moongose from 'mongoose';
 
 class App {
 public app: express.Application;
@@ -7,6 +8,19 @@ public app: express.Application;
 public constructor() {
   this.app = express();
   this.app.use(cors());
+  this.initMongoose();
+  this.connectToDatabase();
+}
+
+private initMongoose(): void {
+  moongose.set('runValidators', true);
+}
+
+private connectToDatabase(): void {
+  moongose.connect('mongodb+srv://KaiqueDM:demetrio08@kaiquedm.ax4e7tq.mongodb.net/?retryWrites=true&w=majority&appName=KaiqueDM', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 }
 
 public listen(port: number): void {
